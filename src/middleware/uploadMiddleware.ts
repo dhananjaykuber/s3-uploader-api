9f68d79e950aab45edb6f3b072d9a9508fded2de
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-07-20 21:45:16 +0530

    feat: Add upload middleware

---
+++ src/middleware/uploadMiddleware.ts
@@ -0,0 +1,23 @@
+// External dependencies
+import { Request } from 'express';
+import multer from 'multer';
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 50 * 1024 * 1024,
+    },
+    fileFilter: (
+        req: Request,
+        file: Express.Multer.File,
+        cb: multer.FileFilterCallback
+    ) => {
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true);
+        } else {
+            cb(new Error('Only image files are allowed'));
+        }
+    },
+});
+
+export const uploadMiddleware = upload.single('photo');
